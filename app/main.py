e02dc58e4ad3f08c9b128caae11eb8ac90ec0a55
Sri Sai Satya Karthikeya Kompella <satya.24bcd7161@vitapstudent.ac.in>
2026-09-25 19:50:36 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,7 +20,10 @@ except Exception as e:
 app = FastAPI(title="IP-SAKTI Sahayak API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ip-sakti-frontend-i4crlu3m8-karthikeya19.vercel.app"],
+    allow_origins=[
+    "https://ip-sakti-frontend-i4crlu3m8-karthikeya19.vercel.app",
+    "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
